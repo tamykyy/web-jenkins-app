fd67fb4bb534cd4546a65abe828ca342b6ad373f
Mykyta Vykhodtsev <deriel20021903@gmail.com>
2023-07-22 11:29:15 +0300

    initial commit

---
+++ src/main/java/edu/tamyky/webjenkinsapp/App.java
@@ -0,0 +1,11 @@
+package edu.tamyky.webjenkinsapp;
+
+public class App {
+
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
